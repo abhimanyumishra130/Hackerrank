ed6ada6cb082e2f8c10e71f430c5b14a801d9fc9
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-04-30 15:39:03 +0530

    Create GradesStudents.java
    grading the students in a manner

---
+++ GradesStudents.java
@@ -0,0 +1,82 @@
+/*
+question link---https://www.hackerrank.com/challenges/grading/problem
+*/
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    int grade_size=grades.size();
+    
+    for(int i=0;i<grade_size;i++){
+        if(grades.get(i)>35){
+        if(grades.get(i)%5==3){
+            int n=grades.get(i);
+            //System.out.print(n);
+            grades.set(i,n+2);
+           // System.out.println(grades.get(i));
+        }
+        if(grades.get(i)%5==4){
+            grades.set(i,grades.get(i)+1);
+        }
+    }
+    }
+    return grades;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+            .map(String::trim)
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining("\n"))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
