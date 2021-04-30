cbbc5da5f947996a35b1e7811fe1fa533e87df09
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-04-30 09:59:49 +0530

    Create BirthdayCake.java
    In this we have to find out number of max size candles

---
+++ BirthdayCake.java
@@ -0,0 +1,64 @@
+/*
+link of question---https://www.hackerrank.com/challenges/birthday-cake-candles/problem
+*/
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<Integer> candles) {
+    // Write your code here
+    int arr_size=candles.size();
+    Collections.sort(candles);
+    int max_size=candles.get(arr_size-1);
+    int sum=0;
+    for(int i=0;i<arr_size;i++){
+        if(candles.get(i)==max_size){
+            sum+=1;
+        }
+    }
+    return sum;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
