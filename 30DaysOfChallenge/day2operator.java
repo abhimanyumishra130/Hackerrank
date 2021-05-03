eb2c5de0387e3f2f8f7990ce782fbd5e7aa29a54
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-03 18:00:37 +0530

    Create day2operator.java

---
+++ 30DaysOfChallenge/day2operator.java
@@ -0,0 +1,40 @@
+//question link-----https://www.hackerrank.com/challenges/30-operators/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+            double tip=((meal_cost*tip_percent)/100);
+            double tax=((meal_cost*tax_percent)/100);
+            double total_cost=meal_cost+tip+tax;
+            //int round_total_cost = (int) Math. round(total_cost);
+            System.out.println(Math.round(total_cost));
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double meal_cost = scanner.nextDouble();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tip_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int tax_percent = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        solve(meal_cost, tip_percent, tax_percent);
+
+        scanner.close();
+    }
+}
