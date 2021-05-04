7b6d69adab0a57272f7493226e20b61115d46e80
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-04 12:59:38 +0530

    Create 4Division.py

---
+++ Python/4Division.py
@@ -0,0 +1,11 @@
+#question link---https://www.hackerrank.com/challenges/python-division/problem
+
+
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+print a//b
+print a/b
