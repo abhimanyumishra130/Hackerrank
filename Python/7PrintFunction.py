4b2141b5bfe541219da1c7fcfad96beb8e2b8ee5
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-04 13:04:59 +0530

    Create 7PrintFunction.py

---
+++ Python/7PrintFunction.py
@@ -0,0 +1,12 @@
+#question link---https://www.hackerrank.com/challenges/python-print/problem
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    s=""
+for i in range(1,n+1):
+    s=s+str(i)
+print(s)
+    
+    
