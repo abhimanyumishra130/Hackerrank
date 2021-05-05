62a558bbf4e057da3eef43db416add95e1a25269
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-05 15:43:26 +0530

    Create day1_Datatype.java

---
+++ 30DaysOfChallenge/day1_Datatype.java
@@ -0,0 +1,43 @@
+//question link---https://www.hackerrank.com/challenges/30-data-types/problem
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        
+        int in=scan.nextInt();
+        double doo=scan.nextDouble();
+         scan.nextLine();
+        String str=scan.nextLine();
+       
+        
+        System.out.println(in+i);
+        System.out.println(d+doo);
+        
+        System.out.print(s+""+str);
+
+        /* Declare second integer, double, and String variables. */
+
+        //* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
