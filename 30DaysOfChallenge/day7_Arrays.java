41e2d1a3890f1fcef36fb01191c55e837ceb592b
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-08 11:24:59 +0530

    Create day7_Arrays.java

---
+++ 30DaysOfChallenge/day7_Arrays.java
@@ -0,0 +1,38 @@
+//question link--https://www.hackerrank.com/challenges/30-arrays/problem
+
+
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+        for(int i=n-1;i>=0;i--){
+                System.out.print(arr.get(i)+" ");
+        }
+
+        bufferedReader.close();
+    }
+}
