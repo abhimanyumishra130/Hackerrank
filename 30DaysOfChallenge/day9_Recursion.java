d7cacb47790c3228e2ec3746f61de2b1f03c88bd
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-10 12:32:39 +0530

    Create day9_Recursion.java

---
+++ 30DaysOfChallenge/day9_Recursion.java
@@ -0,0 +1,50 @@
+//question link--https://www.hackerrank.com/challenges/30-recursion/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+    // Write your code here
+    //int k=1;
+    if(n<=0) { 
+        return 1;
+    }
+    // Base Case n = 1
+    else{
+            return n * factorial(n - 1);
+    }
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
