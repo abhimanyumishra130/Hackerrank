7d271fbb174e706ae12e74dd66ea70c137c483dc
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-12 17:12:31 +0530

    Create BreakingTheRecord.java

---
+++ BreakingTheRecord.java
@@ -0,0 +1,74 @@
+//question link---https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+    // Write your code here
+    int Hscore=scores.get(0);
+    int Lscore=scores.get(0);
+    int Hcount=0;
+    int Lcount=0;
+    
+    for(int i=0;i<scores.size();i++){
+            if(scores.get(i)>Hscore){
+                    Hscore=scores.get(i);
+                    Hcount++;
+            }else if(scores.get(i)<Lscore){
+                    Lscore=scores.get(i);
+                    Lcount++;
+            }
+            
+    }
+    return Arrays.asList(Hcount,Lcount);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] scoresTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> scores = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresTemp[i]);
+            scores.add(scoresItem);
+        }
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
