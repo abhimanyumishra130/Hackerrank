2bc2e948e7d3fc38c9c284c5e3fa71b21a164819
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-12 17:50:17 +0530

    Create SubArrayDivision.java

---
+++ SubArrayDivision.java
@@ -0,0 +1,67 @@
+//question link--https://www.hackerrank.com/challenges/the-birthday-bar/problem?h_r=next-challenge&h_v=zen
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+            
+            int count=0;
+            int a=0;
+            for(int i=m-1;i<s.size();i++){
+                    int sum=0;
+                    for(int j=a;j<a+m;j++){
+                            sum=sum+s.get(j);
+                            //System.out.print(sum);
+                           
+                    }
+                     if(sum==d){
+                    count++;
+            }
+                    a++;
+                   // System.out.println(i);
+            }
+            
+            return count;
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] sItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sItems[i]);
+            s.add(sItem);
+        }
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
