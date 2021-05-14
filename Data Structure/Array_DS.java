4407e059463b1465508a3ed58094a9a72859f308
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-14 17:41:11 +0530

    Update Array_DS.java

--- Data Structure/Array_DS.java
+++ Data Structure/Array_DS.java
@@ -10,18 +10,57 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
+class Result {
 
+    /*
+     * Complete the 'reverseArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+    // Write your code here
+    ArrayList<Integer> arr=new ArrayList<Integer>();
+    for(int i=a.size()-1;i>=0;i--){
+            arr.add(a.get(i));
+            
+    }
+    return arr;
+
+    }
+
+}
 
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < arrCount; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        List<Integer> res = Result.reverseArray(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            bufferedWriter.write(String.valueOf(res.get(i)));
+
+            if (i != res.size() - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
 
         bufferedReader.close();
-        
-        for(int i=1;i<=10;i++){
-                System.out.println(n+" x "+i+" = "+(n*i));
-        } 
+        bufferedWriter.close();
     }
 }
