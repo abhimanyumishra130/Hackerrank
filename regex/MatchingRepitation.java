9009866011ba112bcf6ac130ac0252ed39a578e2
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-05-26 19:23:10 +0530

    Create MatchingRepitation.java

---
+++ regex/MatchingRepitation.java
@@ -0,0 +1,21 @@
+//question link--https://www.hackerrank.com/challenges/matching-x-repetitions/problem
+
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-zA-Z02468]{40}[13579\\s]{5}$"); // Use \\ instead of using \ 
+    
+    }
+}
+
+class Regex_Test {
