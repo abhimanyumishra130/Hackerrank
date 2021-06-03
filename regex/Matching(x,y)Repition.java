061b719e495c09f56bfe57fe6495deef213aef47
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-06-03 18:45:43 +0530

    Create Matching(x,y)Repition.java

---
+++ regex/Matching(x,y)Repition.java
@@ -0,0 +1,31 @@
+//question link--https://www.hackerrank.com/challenges/matching-x-y-repetitions/problem
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {    
+
+    public static void main(String[] args) {
+        
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^\\d{1,2}[a-zA-Z]{3,}\\D{1,3}$"); // Use \\ instead of using \ 
+    
+    }
+}
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+    
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }   
+    
+}
