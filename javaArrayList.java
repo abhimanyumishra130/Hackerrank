beb40f2415f2e8c486816b2f32a994aa5f5e1c37
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-09-10 20:23:49 +0530

    Update javaArrayList.java

--- javaArrayList.java
+++ javaArrayList.java
@@ -5,6 +5,7 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
+    
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
