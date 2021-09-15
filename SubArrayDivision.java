7e7abc3bbc67eca18c3d3a106c9513dc76ba344e
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-09-15 21:05:28 +0530

    Update SubArrayDivision.java

--- SubArrayDivision.java
+++ SubArrayDivision.java
@@ -35,6 +35,7 @@ public class Solution {
 
     }
 
+    
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
