b66078574e2162655ee0b74389d3c7126ccb93ff
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-11-05 18:55:07 +0530

    Create day19_Interface.java

---
+++ 30DaysOfChallenge/day19_Interface.java
@@ -0,0 +1,31 @@
+//https://www.hackerrank.com/challenges/30-interfaces/problem?isFullScreen=false
+
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic{
+   int divisorSum(int n);
+}
+class Calculator implements AdvancedArithmetic {
+    public int divisorSum(int n) {
+            int sum = 0;
+            for(int i=1; i<=n; i++){
+                    if(n%i==0) sum = sum+ i;
+            }
+        return sum;
+    }
+}
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
