d10e25595b951445023af4ee1275cdcb5895090b
Abhimanyu Mishra <83170741+abhimanyumishra130@users.noreply.github.com>
2021-11-06 20:08:39 +0530

    Create BIl_Division.java

---
+++ BIl_Division.java
@@ -0,0 +1,48 @@
+//https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=false
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+        int sum = 0;
+        for(int i=0; i<bill.size(); i++) {
+            sum += i!=k?bill.get(i):0;
+        }
+        System.out.println(((sum/2) == b)?"Bon Appetit": b-(sum/2));
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
